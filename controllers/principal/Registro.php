44d509de3d6d3fd340f01579209acb9862a50dd6
pato <patriciobmx16@gmail.com>
2024-05-16 17:38:09 -0400

    24) Registro Cliente agregado

--- controllers/principal/Registro.php
+++ controllers/principal/Registro.php
@@ -11,4 +11,16 @@ class Registro extends Controller
         $data['subtitle'] = 'Crea tu nueva cuenta';
         $this->views->getView('principal/registro', $data);
     }
+
+    public function crear() {
+        $nombre = strClean($_POST['nombre']);
+        $apellido = strClean($_POST['apellido']);
+        $usuario = strClean($_POST['usuario']);
+        $correo = strClean($_POST['correo']);
+        $clave = strClean($_POST['clave']);
+        $confirmar = strClean($_POST['confirmar']);
+        $hash = password_hash($clave, PASSWORD_DEFAULT);
+        $rol = 2;
+        $this->model->registrarse($nombre, $apellido, $usuario, $correo, $hash, $rol);
+    }
 }
--- views/principal/registro.php
+++ views/principal/registro.php
@@ -9,36 +9,36 @@ include_once 'views/template/portada.php'; ?>
                     <div class="form-heading text-center">
                         <h3 class="form-title">Crear tu cuenta!</h3>
                     </div>
-                    <form method="post">
+                    <form id="formulario" autocomplete="off">
                         <div class="row">
                             <div class="col-md-12 col-sm-12">
                                 <div class="form-group">
-                                    <input class="form-control" type="text" name="name" placeholder="First Name">
+                                    <input class="form-control" type="text" name="nombre" placeholder="Nombre">
                                 </div>
                             </div>
                             <div class="col-md-12 col-sm-12 ">
                                 <div class="form-group">
-                                    <input class="form-control" type="text" name="name" placeholder="Last Name">
+                                    <input class="form-control" type="text" name="apellido" placeholder="Apellido">
                                 </div>
                             </div>
                             <div class="col-md-12 col-sm-12">
                                 <div class="form-group">
-                                    <input class="form-control" type="text" name="name" placeholder="Enter your username">
+                                    <input class="form-control" type="text" name="usuario" placeholder="Usuario">
                                 </div>
                             </div>
                             <div class="col-md-12 col-sm-12">
                                 <div class="form-group">
-                                    <input class="form-control" type="email" name="email" placeholder="Email Address">
+                                    <input class="form-control" type="email" name="correo" placeholder="Correo Electrónico">
                                 </div>
                             </div>
                             <div class="col-md-12 col-sm-12">
                                 <div class="form-group">
-                                    <input class="form-control" type="text" name="password" placeholder="Password">
+                                    <input class="form-control" type="text" name="clave" placeholder="Contraseña">
                                 </div>
                             </div>
                             <div class="col-md-12 col-sm-12 ">
                                 <div class="form-group">
-                                    <input class="form-control" type="text" name="password" placeholder="Confirm Password">
+                                    <input class="form-control" type="text" name="confirmar" placeholder="Confirmar Contraseña">
                                 </div>
                             </div>
                             <div class="col-md-12 col-sm-12 col-xs-12 form-condition">
@@ -46,7 +46,7 @@ include_once 'views/template/portada.php'; ?>
                                     <input type="checkbox" id="chb1">
                                     <label for="chb1">
                                         Estoy de acuerdo con la
-                                        <a href="#">Politica y Privacidad de CanchaClick</a>
+                                        <a href="#">Terminos y Condiciones de CanchaClick</a>
                                     </label>
                                 </div>
                             </div>
